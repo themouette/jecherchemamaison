c8ff58ba900c025026378defb8e63528b3f082ec
Julien Muetton <themouette@gmail.com>
2014-01-30 23:44:04 +0100

    Add fossil and handlebars.

--- src/www/config.js
+++ src/www/config.js
@@ -4,10 +4,21 @@ require.config({
         'vendor': '<%= vendors %>',
         // jQuery defines itself as 'jquery', so you should
         // use this alias too.
-        'jquery': '<%= vendors %>/jquery/jquery'
+        'jquery': '<%= vendors %>/jquery/jquery',
+        'underscore': '<%= vendors %>/underscore/underscore',
+        'backbone': '<%= vendors %>/backbone/backbone',
+        'handlebars': '<%= vendors %>/handlebars/handlebars',
+        'hbars': '<%= vendors %>/requirejs-handlebars/hbars',
+        'fossil': '<%= vendors %>/fossil-core/src',
+        'fossil/views': '<%= vendors %>/fossil-view/src'
     },
     map: {
-        '*': {
-        }
+        'hbars': {'Handlebars': 'handlebars'},
+        '*': {'Handlebars': 'handlebars'}
+    },
+    shim: {
+        'underscore': {exports: '_'},
+        'backbone': {deps: ['underscore', 'jquery'], exports: 'Backbone'},
+        'handlebars': {exports: 'Handlebars'}
     }
 });
