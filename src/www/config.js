643bc58290e950b1e2dc1e18d116b2490b666afd
Julien Muetton <themouette@gmail.com>
2014-02-02 01:04:10 +0100

    Add foundation.

--- src/www/config.js
+++ src/www/config.js
@@ -1,5 +1,6 @@
 require.config({
     baseUrl: 'js/',
+    deps: ['foundation'],
     paths: {
         'vendor': '<%= vendors %>',
         // jQuery defines itself as 'jquery', so you should
@@ -8,17 +9,25 @@ require.config({
         'underscore': '<%= vendors %>/underscore/underscore',
         'backbone': '<%= vendors %>/backbone/backbone',
         'handlebars': '<%= vendors %>/handlebars/handlebars',
-        'hbars': '<%= vendors %>/requirejs-handlebars/hbars',
+        'text': '<%= vendors %>/requirejs-text/text',
         'fossil': '<%= vendors %>/fossil-core/src',
-        'fossil/views': '<%= vendors %>/fossil-view/src'
+        'fossil/views': '<%= vendors %>/fossil-view/src',
+        'foundation': '<%= vendors %>/foundation/js/foundation.min',
+
+        'fossil/views/model': 'views/model'
     },
     map: {
-        'hbars': {'Handlebars': 'handlebars'},
-        '*': {'Handlebars': 'handlebars'}
+        '*': {}
     },
     shim: {
         'underscore': {exports: '_'},
         'backbone': {deps: ['underscore', 'jquery'], exports: 'Backbone'},
-        'handlebars': {exports: 'Handlebars'}
+        'handlebars': {exports: 'Handlebars'},
+        'foundation': {
+            deps: ['jquery'],
+            init: function (foundation) {
+                $(document).foundation();
+            }
+        }
     }
 });
