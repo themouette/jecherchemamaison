77ead7d6afa94149b31e53d0142ffc445769dd8b
Julien Muetton <themouette@gmail.com>
2014-02-04 16:51:50 +0100

    Add a log message for config root.

--- src/config.js
+++ src/config.js
@@ -1,5 +1,8 @@
 var path = require('path')
 var root = path.resolve(__dirname, '..', 'data');
+console.log('-------------------->');
+console.log('Data store: "%s"', root);
+console.log('-------------------->');
 module.exports = {
     "data": {
         "dir": root,
