e9b9f5933f090fdc8c30e1114d71d3bf22e9de60
Julien Muetton <themouette@gmail.com>
2014-02-04 21:20:17 +0100

    Fix config.

--- src/config.js
+++ src/config.js
@@ -1,8 +1,9 @@
-var path = require('path')
+var path = require('path');
 var root = path.resolve(__dirname, '..', 'data');
 console.log('-------------------->');
 console.log('Data store: "%s"', root);
 console.log('-------------------->');
+
 module.exports = {
     "data": {
         "dir": root,
