1359b4a5607f376b6b0595b1a3235557b997bcec
Julien Muetton <themouette@gmail.com>
2014-02-10 14:37:57 +0100

    Migrate from grun-express to grunt-contrib-connect

--- src/server/index-dev.js
+++ src/server/index-dev.js
@@ -1,49 +1,15 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+
+
 var main = require('./index');
-
-var rootDir = path.resolve(path.join(__dirname, '..', '..'));
-var wwwPublicDir = path.join(rootDir, 'public');
-var wwwJsDir = path.join(rootDir, 'src/www');
-var wwwVendorDir = path.join(rootDir, 'bower_components');
-
-console.log(wwwPublicDir, wwwJsDir, wwwVendorDir);
-
-app.use(express.logger('dev'));
-
-app.use('/vendors', express.static(wwwVendorDir));
 app.use(main);
-app.use('/js', express.static(wwwJsDir));
-app.use('/vendors', express.directory(wwwVendorDir));
-app.use('/', express.directory(wwwJsDir));
 
-// I want to place any static content here
-// but I want to define the location of these static content in `grunt-express` options like so:
-//
-// grunt.initConfig({
-//  express: {
-//    livereloadServer: {
-//      server: path.resolve(__dirname, 'server'),
-//      bases: [path.resolve(__dirname, 'public'), path.resolve(__dirname, '.tmp')],
-//      livereload: true,
-//      serverreload: true
-//    },
-//    productionServer: {
-//      server: path.resolve(__dirname, 'server'),
-//      bases: path.resolve(__dirname, 'dist')
-//    }
-//  }
-// });
-// Notice the name of the following middleware function
-app.use(function staticsPlaceholder(req, res, next) {
-  return next();
-});
-
-
-// here is where I want my dynamic middlewares be loaded
-app.use(function middlewarePlaceholder(req, res, next) {
-  return next();
-});
 
 module.exports = app;
+if (!module.parent) {
+    var port = app.get('port') || process.env.PORT || 8080;
+    app.listen(port);
+    console.log('Express started on port '+port);
+}
--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var app = express();
-var path = require('path');
 
-var rootDir = path.resolve(path.join(__dirname, '..', '..'));
-var wwwPublicDir = path.join(rootDir, 'public');
-app.use('/', express.static(wwwPublicDir));
+// Your code comes here.
 
 module.exports = app;
